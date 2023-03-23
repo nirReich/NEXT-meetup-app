855fef9cbebe91929ca416c652f139dd568de2f5
nirReich <reich.nir@gmail.com>
2023-03-23 19:13:50 +0200

    added static props and api folder to next

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -14,4 +14,38 @@ function MeetupDetails() {
   );
 }
 
+export async function getStaticPaths(params) {
+  return {
+    fallback:false,
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  const meetupId = context.params.meetupId;
+  return {
+    props: {
+      meetupData: {
+        image:
+          "https://i0.wp.com/www.touristisrael.com/wp-content/uploads/2021/01/Tel-Aviv-Travel.jpg?fit=2000%2C1381&ssl=1",
+        id: meetupId,
+        title: "fist meetup",
+        address: "some street,5 tlv israel",
+        description: "this is my first meetup",
+      },
+    },
+  };
+}
+
 export default MeetupDetails;
--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,48 @@
 import MeetupList from "@/components/meetups/MeetupList";
-import React from "react";
-import { useState } from "react";
-import { useEffect } from "react";
 
 const dummy_data = [
   {
     id: "m1",
     image:
       "https://i0.wp.com/www.touristisrael.com/wp-content/uploads/2021/01/Tel-Aviv-Travel.jpg?fit=2000%2C1381&ssl=1",
-    title: "tel aviv meet 1",
-    address: "some addres, israel",
+    title: "Tel-Aviv meet 1",
+    address: "some address, israel",
   },
   {
     id: "m2",
     image:
       "https://i0.wp.com/www.touristisrael.com/wp-content/uploads/2021/01/Tel-Aviv-Travel.jpg?fit=2000%2C1381&ssl=1",
-    title: "tel aviv meet 2",
-    address: "some addres, israel 2",
+    title: "Tel-Aviv meet 2",
+    address: "some address, israel 2",
   },
 ];
 
-
-function HomePage() {
-  const [meetupsList,setMeetupsList] = useState([])
-  
-  useEffect(()=>{
-    setMeetupsList(dummy_data)
-  },[])
-
+function HomePage({ meetups }) {
   return (
     <div>
-      <MeetupList meetups={meetupsList} />
+      <MeetupList meetups={meetups} />
     </div>
   );
 }
+
+export async function getStaticProps() {
+  // do something
+  return {
+    props: {
+      meetups: dummy_data,
+    },
+    revalidate: 10,
+  };
+}
+
+// export async function getServerSideProps(context) {
+//   const req = context.req;
+//   const res = context.res;
+//   return {
+//     props: {
+//       meetups: dummy_data,
+//     },
+//   };
+// }
 
 export default HomePage;
