395bdbc170e5ef9e007d80efd634d78d47f6c96e
nirReich <reich.nir@gmail.com>
2023-03-25 20:55:28 +0300

    single meetup renders on server

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,48 +1,58 @@
 import React from "react";
+import { MongoClient, ObjectId } from "mongodb";
 import MeetupDetail from "../../components/meetups/MeetupDetail";
 
-function MeetupDetails() {
+function MeetupDetails({ meetupData }) {
   return (
     <>
       <MeetupDetail
-        image="https://i0.wp.com/www.touristisrael.com/wp-content/uploads/2021/01/Tel-Aviv-Travel.jpg?fit=2000%2C1381&ssl=1"
-        title="fist meetup"
-        address="some street,5 tlv israel"
-        description="this is my first meetup"
+        image={meetupData.image}
+        title={meetupData.title}
+        address={meetupData.address}
+        description={meetupData.description}
       />
     </>
   );
 }
 
 export async function getStaticPaths(params) {
+  const client = await MongoClient.connect(
+    "mongodb+srv://nir:7281161@meetups.7fwfjef.mongodb.net/?retryWrites=true&w=majority"
+  );
+  const db = client.db();
+
+  const meetupCollection = db.collection("meetups");
+  const meetups = await meetupCollection.find({}, { _id: 1 }).toArray();
+  client.close();
   return {
-    fallback:false,
-    paths: [
-      {
-        params: {
-          meetupId: "m1",
-        },
-      },
-      {
-        params: {
-          meetupId: "m2",
-        },
-      },
-    ],
+    fallback: false,
+    paths: meetups.map((meetup) => ({
+      params: {
+        meetupId: meetup._id.toString()
+      }
+    })),
   };
 }
 
 export async function getStaticProps(context) {
   const meetupId = context.params.meetupId;
+  const client = await MongoClient.connect(
+    "mongodb+srv://nir:7281161@meetups.7fwfjef.mongodb.net/?retryWrites=true&w=majority"
+  );
+  const db = client.db();
+
+  const meetupCollection = db.collection("meetups");
+  const selectedMeetup = await meetupCollection.findOne({
+    _id: new ObjectId(meetupId),
+  });
   return {
     props: {
       meetupData: {
-        image:
-          "https://i0.wp.com/www.touristisrael.com/wp-content/uploads/2021/01/Tel-Aviv-Travel.jpg?fit=2000%2C1381&ssl=1",
-        id: meetupId,
-        title: "fist meetup",
-        address: "some street,5 tlv israel",
-        description: "this is my first meetup",
+        id: selectedMeetup._id.toString(),
+        title: selectedMeetup.title,
+        image:selectedMeetup.image,
+        address: selectedMeetup.address,
+        description: selectedMeetup.description,
       },
     },
   };
